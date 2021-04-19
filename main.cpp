1c3115565ff9cdc3e412631d21ec055c071fb93f
AliR Namvar <alirnamvar1999@gmail.com>
2021-04-19 16:23:14 +0430

    read one game info in right form.

--- main.cpp
+++ main.cpp
@@ -2,46 +2,62 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <vector>
 
 using namespace std;
 
-// ***** Prototypes ****** //
-void read_game_score(void);
+// ***** Start of Prototypes ****** //
+vector<string> read_game_score(void);
 bool check_correct_team_input(string);
+bool check_correct_score_input(string);
+
+// *****   End of Prototypes ****** //
 
 
-int main(int argc, char const *argv[])
-{
-    // std::cout << "Hello boy!";
-
-
-    read_game_score();
+int main(int argc, char const *argv[]) {
+   
+    vector<string> one_game_info = read_game_score();
+    cout << "teams: " << one_game_info[0] << "\tscores: " << one_game_info[1] <<endl;
     return 0;
 }
 
 
 
-void read_game_score(void) {
+vector<string> read_game_score(void) {
     
-    string user_input{};
-    bool team_input_correctness = false;
-    cout << "Which teams have a play? (enter team A & B in \"A - B\" form)\n";
     string team_inputs{};
-    getline(cin, team_inputs);
+    string score_inputs{};
 
-    team_input_correctness = check_correct_team_input(team_inputs);
-    cout << team_input_correctness;
-    /*ofstream read_from_cli1;
-    read_from_cli1.open("scores.txt", ios::app);
-    read_from_cli1 << input;
-    read_from_cli1.close();*/
+    // Try to get teams and score in right format
+    do {
+        cout << "Which teams have a play? (enter team A & B in \"A - B\" form)\n";
+        getline(cin, team_inputs);
+    }
+    while (!check_correct_team_input(team_inputs));
 
+    do {
+        cout << "What is the score? (enter team's score A & B in \"A - B\" form)\n";
+        getline(cin, score_inputs);
+    }
+    while (!check_correct_score_input(score_inputs));
+
+    // returning teams and score inputs
+    vector<string> output{};
+    output.push_back(team_inputs);
+    output.push_back(score_inputs);
+    return output;
 }
-
 
 bool check_correct_team_input(string check) {
     
     if (regex_match (check, regex(("^.* - .*$"))))
+        return true;
+    return false;
+}
+
+bool check_correct_score_input(string check) {
+    
+    if (regex_match (check, regex(("^[0-9]+ - [0-9]+$"))))
         return true;
     return false;
 }
