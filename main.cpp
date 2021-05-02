3ad9a9de13ac0245ae60a18898a2b370a32df0ec
AliR Namvar <alirnamvar1999@gmail.com>
2021-05-02 18:11:40 +0430

    get single info and save in game_informations.csv

--- main.cpp
+++ main.cpp
@@ -6,24 +6,55 @@
 
 using namespace std;
 
-// ***** Start of Prototypes ****** //
+// ***** Start Prototypes  ****** //
+
 vector<string> read_game_score(void);
 bool check_correct_team_input(string);
 bool check_correct_score_input(string);
 bool check_correct_week(string);
-// ***** End of Prototypes   ****** //
+void write_gameinfo_2_file(vector<string>, string del = "-");
+
+// ***** End of Prototypes ****** //
+
 
 // ***** Start of Global Variables ***** //
-int team_numbers = 16;
 // ***** End of Global Variables   ***** //
 
 int main(int argc, char const *argv[]) {
 
     vector<string> one_game_info = read_game_score();
-    cout << "teams: " << one_game_info[0] << "\tscores: " << one_game_info[1] <<endl;
+    cout << "teams: " << one_game_info[0] << "\tscores: " << one_game_info[1] << "\tweek: " << one_game_info[2] <<endl;
+
+    write_gameinfo_2_file(one_game_info);
     return 0;
 }
 
+void write_gameinfo_2_file(vector<string> game_info, string del) {
+
+    fstream myfile("game_informations.csv", ios::app);
+    
+    myfile << game_info[2] << ",";
+    string s = game_info[0];
+    int start = 0;
+    int end = s.find(del);
+    while (end != -1) {
+        myfile << s.substr(start, end - start) << ",";
+        start = end + del.size();
+        end = s.find(del, start);
+    }
+    myfile << s.substr(start, end - start) << ",";
+
+    s = game_info[1];
+    start = 0;
+    end = s.find(del);
+    while (end != -1) {
+        myfile << s.substr(start, end - start) << ",";
+        start = end + del.size();
+        end = s.find(del, start);
+    }
+    myfile << s.substr(start, end - start) << endl;
+    myfile.close();
+}
 
 vector<string> read_game_score(void) {
     
@@ -59,10 +90,11 @@ vector<string> read_game_score(void) {
     return output;
 }
 
+// Start Check correctness functions  //
 
 bool check_correct_team_input(string check) {
     
-    if (regex_match (check, regex(("^[A-Za-z]+-[A-Za-z]+$"))))
+    if (regex_match (check, regex(("^[A-Za-z ]+-[A-Za-z ]+$"))))
         return true;
     return false;
 }
@@ -80,3 +112,5 @@ bool check_correct_score_input(string check) {
         return true;
     return false;
 }
+
+// End of Check correctness functions //
